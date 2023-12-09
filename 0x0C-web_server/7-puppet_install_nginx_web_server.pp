c3db2a607860fb9d135334d0720bf88d29b66cb8
Iyanuloluwa Emmanuel Adigun <125540335+Memmalino@users.noreply.github.com>
2023-12-09 02:48:54 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,7 +3,7 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'aaaaa':
+file_line { 'redirection-301':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
