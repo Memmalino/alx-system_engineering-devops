59a5498bddab79c62504149a1cf1f256e048ad47
memmalino <memmalino@gmail.com>
2024-01-02 17:06:32 +0000

    uploaded

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+This module uses https://jsonplaceholder.typicode.com along with an employee ID to
+return information about the employee's todo list progress
+Export this data to JSON
+"""
+import json
+import re
+import requests
+import sys
+
+
+API_URL = 'https://jsonplaceholder.typicode.com'
+"""The API's URL."""
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            todos_res = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_res.get('username')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            with open('{}.json'.format(id), 'w') as file:
+                user_data = list(map(
+                    lambda x: {
+                        'task': x.get('title'),
+                        'completed': x.get('completed'),
+                        'username': user_name
+                    },
+                    todos
+                ))
+                users_data = {
+                    '{}'.format(id): user_data
+                }
+                json.dump(users_data, file)
