b4e3787bcf47c229ef9c9d5b56657f5f690abef0
memmalino <memmalino@gmail.com>
2024-01-16 23:52:14 +0000

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""get a lit of suscribber on a subreddit"""
+"""Fetch number of subscribers of a subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """number of suscribers"""
-    if subreddit is None or type(subreddit) != str:
-        return (0)
-    url = "http://www.reddit.com/r/{}/about.json".format(
-            subreddit)
-    headers = {'User-Agent': 'API project'}
-    response = requests.get(url, headers=headers)
-    if (response.status_code != 200):
-        return (0)
-    sus = response.json().get("data").get('subscribers', 0)
-    return (sus)
+    """get number of subscribers of a subreddit
+    """
+    data = {
+        'User-agent': 'Iamabot'
+    }
+    r = requests.get("https://www.reddit.com/r/{}/about.json"
+                     .format(subreddit),
+                     headers=data,
+                     allow_redirects=False
+                     )
+    if r.status_code != 200 or r.json().get('kind') != 't5':
+        return 0
+    else:
+        return r.json().get('data').get('subscribers')
+
