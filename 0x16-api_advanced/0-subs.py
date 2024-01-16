fce4694574906b32dfe93b1573a7a147923371ff
Memmalino <memmalino@gmail.com>
2024-01-16 23:47:16 +0000

    changes made to 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""Fetch number of subscribers of a subreddit
-"""
+"""get a lit of suscribber on a subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """get number of subscribers of a subreddit
-    """
-    data = {
-        'User-agent': 'Iamabot'
-    }
-    r = requests.get("https://www.reddit.com/r/{}/about.json"
-                     .format(subreddit),
-                     headers=data,
-                     allow_redirects=False
-                     )
-    if r.status_code != 200 or r.json().get('kind') != 't5':
-        return 0
-    else:
-        return r.json().get('data').get('subscribers')
+    """number of suscribers"""
+    if subreddit is None or type(subreddit) != str:
+        return (0)
+    url = "http://www.reddit.com/r/{}/about.json".format(
+            subreddit)
+    headers = {'User-Agent': 'API project'}
+    response = requests.get(url, headers=headers)
+    if (response.status_code != 200):
+        return (0)
+    sus = response.json().get("data").get('subscribers', 0)
+    return (sus)
